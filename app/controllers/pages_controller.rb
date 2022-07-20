c2d7fb0f1a48a01abf9a5a24d5d39fcf50b4dd5a
Soledium <nescobar3@hotmail.com>
2022-07-20 13:04:00 -0400

    commit dos y tres

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,15 @@
 class PagesController < ApplicationController
+
+    def one
+        
+    end
+
+    def two
+        
+    end
+
+    def three
+        
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+    root "pages#one"
+    get 'pages/one'
+    get 'pages/two'
+    get 'pages/three'
+
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
